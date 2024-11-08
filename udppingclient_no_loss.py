30ce789c3fe5b3c72e43f5172f9cb374b0cee2f7
dangkyle64 <kyledang64@gmail.com>
2024-11-08 15:30:05 -0800

    initial commit
    author: dangkyle64
    date: 11/8/2024

---
+++ udppingclient_no_loss.py
@@ -0,0 +1,16 @@
+#The client program should:
+# send the ping message using UDP
+# print the response message from server if any was received
+# calculate and print the round-trip time (RTT), in milliseconds, of each packet if the server
+#responses
+# otherwise, print “Request timed out”
+# provide a sumAlice report at the end (of all pings) which includes:
+# minimum RTT in milliseconds,
+# maximum RTT in milliseconds,
+# average RTT in milliseconds,
+# percentage packet loss rate
+
+#example
+# firstname ping_number date_and_time
+# Alice 1: server reply: Alice 1 Fri Sep 13 09:00:15 2024, RTT = 3.99 ms
+# Alice 2: server reply: Alice 2 Fri Sep 13 09:00:15 2024, RTT = 0.00 ms
